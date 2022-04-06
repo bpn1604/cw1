1432fb48f021d4b05e6b8c40c52e7d892e7f5d39
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2022-04-07 00:13:31 +0530

    one final

---
+++ src/component/First.jsx
@@ -0,0 +1,20 @@
+const First = () =>{
+    return <>
+    <h1>Mobile Operating System</h1>
+    <ul>Andriod</ul>
+    <ul>Blackberry</ul>
+    <ul>iPhone</ul>
+    <ul>Windows Phone</ul>
+    </>
+}
+export const Second = () =>{
+    return <>
+    <h1>Mobile Manufacturer</h1>
+    <ul>Samsung</ul>
+    <ul>HTC</ul>
+    <ul>Micromax</ul>
+    <ul>Apple</ul>
+    </>
+}
+
+export default First
